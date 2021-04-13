d5f830bd64778e4d5782d6a22b3ff2ee83f92849
Louis Bertson <louisbertson@gmail.com>
2021-04-13 00:09:13 +0000

    added missing handleUserDisconectioin helper an fixed server starting craches

--- public/js/chat_ui.js
+++ public/js/chat_ui.js
@@ -28,10 +28,49 @@ function processUserInput(chatApp,socket){
 var socket = io.connect();
 
 $(document).ready(function(){
-    var chatApp = new Chat(socket);
+            var chatApp = new Chat(socket);
 
 
-socket.on(function(){
-    
-})
+        socket.on('nameResult',function(result){
+        var message;
+        if(result.success){
+            message = "You are now knows as "+ result.name +"."    
+        }else{
+            message = result.message;
+        }
+        $('#messages').append(divSystemContentElement(message));
+
+        });
+        socket.on('joinResult', function(result){
+                $("#room").text(result.room);
+                $('#messages').append(divSystemContentElement('Room Changed'));
+        });
+
+        socket.on('message',function(message){
+            var newElement = $('<div></div>').text(message.text);
+            $('#message').append(newElement);
+        });
+        socket.on('rooms',function(rooms){
+            $('#room-list').empty();
+        for (var room in rooms){
+            room = room.substring(1, room.length);
+            if(room != ' '){
+                $('#room-list').append(divEscapedContentElement(room));
+            }
+        }
+        $('#room-list div').click(function(){
+        chatApp.processCommand('/join '+ $(this).text());
+        $('#send-message').focus();
+        })
+        })
+
+        setInterval(function(){
+            socket.emit('rooms');
+        },1000);
+        $('#send-message').focus();
+
+        $('#send-form').sudmit(function(){
+            processUserInput(chatApp,socket);
+            return false;
+        })
 })
